85068ff2ea150b8e3ef66b171cfac6aa121c9c39
Matthias Wessendorf <matzew@apache.org>
2011-07-02 15:26:01 +0200

    adding first file..

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sample")]
+[assembly: AssemblyCopyright("Copyright 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This is not work... Hrm......
+[assembly: InternalsVisibleTo("Sample")]
+
+[assembly: AssemblyVersion("2.2.8.0")]
+[assembly: AssemblyFileVersion("2.2.8.0")]
+[assembly: AssemblyInformationalVersion("2.2.8.0")]
