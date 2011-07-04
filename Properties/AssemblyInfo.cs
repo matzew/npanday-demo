588192a85e8f04f3b9168f5f27c1699ae4cdc9f5
Matthias Wessendorf <matzew@apache.org>
2011-07-04 12:45:19 +0200

    updating ASInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,8 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // This is not work... Hrm......
-[assembly: InternalsVisibleTo("Sample")]
-
-[assembly: AssemblyVersion("2.2.8.0")]
-[assembly: AssemblyFileVersion("2.2.8.0")]
-[assembly: AssemblyInformationalVersion("2.2.8.0")]
+[assembly: InternalsVisibleTo("Sample, PublicKey=002400000480000094000000060200000024000052534131000400001100000051d2e0b45f771fbbd89b5ef2df71dc4e1610aeb72da032764f43b9c8800cfbeb0839d6d11ac014c76e89fc5e961f41fa696a2d6acb3b2ec3e5ac78d8a525cc65e92a2014ee10efd9ed39d96d04ddefc501029e0f48ad3a9831c00b7356dc01cfdf23eb2af9f24f9ad56e50f130783781a56bd0411991668e64b736df9a5d3896")]
